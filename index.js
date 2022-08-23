dbbd1150b1892cbee46fcedadf71b793a92e9759
HaiNamOwO <daothimaidung080977@gmail.com>
2022-08-23 19:32:38 +0700

    new files

---
+++ index.js
@@ -0,0 +1,18 @@
+let count = 0;
+
+document.getElementById("num").innerHTML = count;
+
+document.getElementById("decrease").onclick = function(){
+  count-=1;
+  document.getElementById("num").innerHTML = count;
+}
+
+document.getElementById("reset").onclick = function(){
+  count=0;
+  document.getElementById("num").innerHTML = count;
+}
+
+document.getElementById("increase").onclick = function(){
+  count+=1;
+  document.getElementById("num").innerHTML = count;
+}
