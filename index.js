c83dc1830faf3321d00a45f009786ffc70197e4c
HaiNamOwO <daothimaidung080977@gmail.com>
2022-08-29 15:42:12 +0700

    update1

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 let count = 0;
+let time = 0.00;
 
-document.getElementById("num").innerHTML = count;
 
-document.getElementById("decrease").onclick = function(){
-  count-=1;
+start();
+
+const timer = setInterval(up,10);
+  function up(){
+    if (count!=0){
+      time += 0.01;
+      time = Math.round(time*100)/100;
+      document.getElementById("clock").innerHTML = time;
+      if (time >= 10){
+        clearInterval(timer);
+        alert(`your CPS is ${count/time} clicks per second`);
+        count=0;
+        time=0;
+        document.getElementById("clock").innerHTML = time;
+        document.getElementById("num").innerHTML = count;
+        
+      }
+    }
+  }
+  
+  function start(){
+    document.getElementById("clock").innerHTML = time;
+
   document.getElementById("num").innerHTML = count;
+
+  document.getElementById("increase").onclick = function(){
+    count+=1;
+    document.getElementById("num").innerHTML = count;
 }
-
-document.getElementById("reset").onclick = function(){
-  count=0;
-  document.getElementById("num").innerHTML = count;
-}
-
-document.getElementById("increase").onclick = function(){
-  count+=1;
-  document.getElementById("num").innerHTML = count;
 }
